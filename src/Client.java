084c396fa7ad62fdb26fbbb22da1e12931b1c0d0
ColinearProductions <r.becheru@lancaster.ac.uk>
2015-11-29 17:09:03 +0000

    Initial state of client class

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,21 @@
-/**
- * Created by rbech on 29/11/2015.
- */
+import java.rmi.Naming;
+
+
 public class Client {
+
+    public ServerInterface serverInterface;
+
+    public Client() {
+        try {
+            serverInterface = (ServerInterfaceImplementation) Naming.lookup("rmi://localhost/AuctionServer");
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args){
+
+    }
 }
